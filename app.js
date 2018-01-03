efd467981a3981dabe2fc8c0240ecae7c51b6b89
Tyler Mustard <tjm@Tylers-MacBook-Air.local>
2018-01-03 13:38:27 -0700

    commit

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log('hiiiii')
+const apiURL = 'https://fathomless-chamber-33616.herokuapp.com/'
+
+var characterData
+var characterSelected
+
+
+fetch(apiURL)
+    .then(response => response.json())
+    .then(response => {
+        characterData = response
+        console.log(characterData)
+
+    })
+
+function setCharacter(sel) {
+    characterSelected = sel.options[sel.selectedIndex].text
+}
+
+document.querySelector('#dropD').addEventListener('submit', function(event){
+    console.log('working')
+    event.preventDefault()
+    populateDescription()
+})
+
+function populateDescription() {
+    var descriptionP = document.querySelector('.description-content')
+    console.log(descriptionP)
+    characterData.map(item => {
+        if (item.name === characterSelected) {
+            var moveList = item.move1
+            descriptionP.innerHTML = moveList
+        }
+    })
+}
