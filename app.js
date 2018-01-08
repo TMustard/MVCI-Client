d4ea66dd751f71c6aa37fadcdeee57e8f45e6f3a
Tyler Mustard <tjm@Tylers-MacBook-Air.local>
2018-01-08 13:48:28 -0700

    commit

--- app.js
+++ app.js
@@ -1,34 +1,41 @@
-const apiURL = 'https://fathomless-chamber-33616.herokuapp.com/'
-
-var characterData
-var characterSelected
-
+// const apiURL = "https://fathomless-chamber-33616.herokuapp.com/"
+const apiURL = "http://localhost:8080"
+var characterData;
+var characterSelected;
 
 fetch(apiURL)
-    .then(response => response.json())
-    .then(response => {
-        characterData = response
-        console.log(characterData)
-
-    })
+  .then(response => response.json())
+  .then(response => {
+    characterData = response.moves;
+    console.log(characterData);
+  });
 
 function setCharacter(sel) {
-    characterSelected = sel.options[sel.selectedIndex].text
+  characterSelected = sel.options[sel.selectedIndex].text;
+  console.log(characterSelected);
+  return characterSelected;
 }
 
-document.querySelector('#dropD').addEventListener('submit', function(event){
-    console.log('working')
-    event.preventDefault()
-    populateDescription()
-})
+document.querySelector("#dropD").addEventListener("submit", function(event){
+  event.preventDefault();
+  populateDescription();
+});
 
+
+// function populateDescription() {
+//     // var descriptionP = document.querySelector('.description-content')
+//     if( === characterSelected) {
+//         console.log('hi')
+//     }
+// }
 function populateDescription() {
-    var descriptionP = document.querySelector('.description-content')
-    console.log(descriptionP)
-    characterData.map(item => {
-        if (item.name === characterSelected) {
-            var moveList = item.move1
-            descriptionP.innerHTML = moveList
-        }
-    })
+  var descriptionP = document.querySelector(".description-content");
+  console.log(characterData);
+  characterData.map(item => {
+    console.log(item)
+    if (item.name === characterSelected) {
+      var moveList = item.move1;
+      descriptionP.innerHTML = moveList;
+    }
+  });
 }
