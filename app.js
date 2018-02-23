e3e33e983dc25baa254a05d8436faf64396c3b53
Tyler Mustard <tjm@Tylers-MacBook-Air.local>
2018-02-22 17:55:36 -0700

    c

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const apiURL = "https://fathomless-chamber-33616.herokuapp.com/";
+const apiURL = "https://fathomless-chamber-33616.herokuapp.com";
 // const apiURL = "http://localhost:8000";
-var moveData;
+var moveData = [];
 var comboData;
 var characterData = [];
 var characterSelected;
 
-fetch(`${apiURL}moves`)
+fetch(`${apiURL}/moves`)
   .then(response => response.json())
   .then(response => {
     moveData = response.moves;
   });
 
-fetch(`${apiURL}combos`)
+fetch(`${apiURL}/combos`)
   .then(response => response.json())
   .then(response => {
     comboData = response.combos;
@@ -20,9 +20,13 @@ fetch(`${apiURL}combos`)
     }
   });
 
-alert(
-  "This is an app for players of Marvel vs. Capcom Infinite that shows character moves and combos! This site is formatted for mobile. Also, the data is incomplete at the moment. Select Nova for an example of how a complete list of moves and combos would look."
-);
+function showInfo() {
+  if ((document.querySelector(".aboutP").style.display = "none")) {
+    document.querySelector(".aboutP").style.display = "inline";
+  } else {
+    document.querySelector(".aboutP").style.display = "none";
+  }
+}
 
 function setCharacter(sel) {
   characterSelected = sel.options[sel.selectedIndex].text;
